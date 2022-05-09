bbf62789341d8ca5b71c2212bcd7064b313b85bb
noah.funtanilla <noah.funtanilla@cognizant.com>
2022-05-09 12:55:13 -0600

    initial setup

---
+++ src/test/java/com/cognizant/Post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.Post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
