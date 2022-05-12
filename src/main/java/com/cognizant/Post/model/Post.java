a0cea5a7641030d91018051d2586e6898309af4b
Carlos Hernandez Espinoza <carlos.hernandezespinoza@cognizant.com>
2022-05-12 14:53:15 -0500

    Created data.sql and set up database to be populated

--- src/main/java/com/cognizant/Post/model/Post.java
+++ src/main/java/com/cognizant/Post/model/Post.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @AllArgsConstructor
-@Table
+@Table(name = "Posts")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,8 +22,17 @@ public class Post {
     private String img;
     @Column(name = "description")
     private String description;
-    @Column(name = "created_on")
+
+
+    @Column(name = "created_on", columnDefinition = "DATE")
     private LocalDate createdOn;
-
-
+    // LocalDate and Date are different
+    // LocalDate is what we're using for this project
+    // SQL will return a Date
+    //
+    // Date -> LocalDate
+    // {Date object}.toLocalDate();
+    //
+    // LocalDate -> Date
+    // Date.valueOf({LocalDate object});
 }
