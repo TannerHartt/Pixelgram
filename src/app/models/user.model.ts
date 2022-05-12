a9508bf84d0a463f518bd8952ecb267a730815b1
Justin Freeman <justin.freeman@cognizant.com>
2022-05-12 16:25:10 -0500

    Front end constructors and feed posts started.

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+//User constructor for posts
+export class User {
+  constructor(
+  public id: number,
+  public name: string,
+  public username: string,
+  public email: string,
+  public avatar: string,
+  public bio: string,
+  public images: number,
+  public likes: number,
+  public created: Date)
+}
