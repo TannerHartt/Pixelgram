b5ae11e2a580a6f8ed442f512af004fdf2877ad9
Allen <2185573@cognizant.com>
2022-05-12 13:29:02 -0400

    Added tslib folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostUIComponent } from './post-ui/post-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostUIComponent
   ],
   imports: [
     BrowserModule
