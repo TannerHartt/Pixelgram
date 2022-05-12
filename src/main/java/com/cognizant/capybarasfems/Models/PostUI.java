041eba5189bdeba3ee5245041362a5ae8a51706a
Carlos Hernandez Espinoza <carlos.hernandezespinoza@cognizant.com>
2022-05-12 11:40:52 -0500

    Refactoring PostUI

--- src/main/java/com/cognizant/capybarasfems/Models/PostUI.java
+++ src/main/java/com/cognizant/capybarasfems/Models/PostUI.java
@@ -1,62 +1,20 @@
 package com.cognizant.capybarasfems.Models;
 
-import java.time.LocalDate;
-
 public class PostUI {
-    private long id;
-    private User user;
-    private String img;
-    private String description;
-    private LocalDate createdOn;
+    private Post post;
     private PageOfItems<Comment> comments;
 
-    public PostUI(long id, User user, String img, String description, LocalDate createdOn, PageOfItems<Comment> comments) {
-        this.id = id;
-        this.user = user;
-        this.img = img;
-        this.description = description;
-        this.createdOn = createdOn;
+    public PostUI(Post post, PageOfItems<Comment> comments) {
+        this.post = post;
         this.comments = comments;
     }
 
-    public long getId() {
-        return id;
+    public Post getPost() {
+        return post;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(LocalDate createdOn) {
-        this.createdOn = createdOn;
+    public void setPost(Post post) {
+        this.post = post;
     }
 
     public PageOfItems<Comment> getComments() {
--- src/test/java/com/cognizant/capybarasfems/ModelTests/PostUITest.java
+++ src/test/java/com/cognizant/capybarasfems/ModelTests/PostUITest.java
@@ -1,9 +1,6 @@
 package com.cognizant.capybarasfems.ModelTests;
 
-import com.cognizant.capybarasfems.Models.Comment;
-import com.cognizant.capybarasfems.Models.PageOfItems;
-import com.cognizant.capybarasfems.Models.PostUI;
-import com.cognizant.capybarasfems.Models.User;
+import com.cognizant.capybarasfems.Models.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,12 +15,14 @@ public class PostUITest {
     @BeforeEach
     public void setup() {
         postUI = new PostUI(
-                1,
-                new User(1,"karlos","pimage"),
-                "image",
-                "Hello, World!",
-                LocalDate.of(2022, 5, 11),
-                new PageOfItems<Comment>(
+                new Post(
+                        1,
+                        1,
+                        "img",
+                        "Hello",
+                        LocalDate.of(2022, 5, 11)
+                ),
+                new PageOfItems<>(
                         new ArrayList<>(),
                         true,
                         30
@@ -33,59 +32,28 @@ public class PostUITest {
 
     @Test
     public void testConstructorAndGetters() {
-       User tempUser=  new User(2,"bob","pimage2");
-       PageOfItems<Comment> tempComment = new PageOfItems<Comment>(new ArrayList<>(),true,30);
+        Post tempPost = new Post(2, 2, "image2", "Hey", LocalDate.of(2000, 1, 1));
+        PageOfItems<Comment> tempComment = new PageOfItems<>(new ArrayList<>(), true, 30);
         postUI = new PostUI(
-                2,
-                tempUser,
-                "image2",
-                "Hey",
-                LocalDate.of(2000, 1, 1),
+                tempPost,
                 tempComment
         );
 
-        Assertions.assertEquals(2, postUI.getId());
-        Assertions.assertEquals(tempUser, postUI.getUser());
-        Assertions.assertEquals("image2", postUI.getImg());
-        Assertions.assertEquals("Hey", postUI.getDescription());
-        Assertions.assertEquals(LocalDate.of(2000,1,1), postUI.getCreatedOn());
+        Assertions.assertEquals(tempPost, postUI.getPost());
         Assertions.assertEquals(tempComment, postUI.getComments());
     }
 
     @Test
-    public void testSetId() {
-        postUI.setId(2);
-        Assertions.assertEquals(2, postUI.getId());
+    public void testSetPost() {
+        Post tempPost = new Post(2, 2, "image2", "Hey", LocalDate.of(2000, 1, 1));
+        postUI.setPost(tempPost);
+
+        Assertions.assertEquals(tempPost, postUI.getPost());
     }
 
     @Test
-    public void testSetUser() {
-        User tempUser = new User(2,"bob","pimage2");
-        postUI.setUser(tempUser);
-        Assertions.assertEquals(tempUser, postUI.getUser());
-    }
-
-    @Test
-    public void testSetImg() {
-        postUI.setImg("image2");
-        Assertions.assertEquals("image2", postUI.getImg());
-    }
-
-    @Test
-    public void testSetDescription() {
-        postUI.setDescription("Hey");
-        Assertions.assertEquals("Hey", postUI.getDescription());
-    }
-
-    @Test
-    public void testSetCreatedOn() {
-        postUI.setCreatedOn(LocalDate.of(2000, 1, 1));
-        Assertions.assertEquals(LocalDate.of(2000, 1, 1), postUI.getCreatedOn());
-    }
-
-    @Test
-    public void TestSetComments(){
-       PageOfItems<Comment> tempComment = new PageOfItems<Comment>(new ArrayList<>(), true,30);
+    public void TestSetComments() {
+        PageOfItems<Comment> tempComment = new PageOfItems<>(new ArrayList<>(), true, 30);
         postUI.setComments(tempComment);
         Assertions.assertEquals(tempComment, postUI.getComments());
     }
