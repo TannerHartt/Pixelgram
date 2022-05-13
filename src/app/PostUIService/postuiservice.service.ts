9136d6e6cc286cbad15b67bc16534bd6d0a85d9f
Allen <2185573@cognizant.com>
2022-05-13 15:17:00 -0400

    Updated Post UI Service

--- src/app/PostUIService/postuiservice.service.ts
+++ src/app/PostUIService/postuiservice.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostuiserviceService {
-  /**HTTPClient injection */
-  constructor() { }
+  /**HTTPClient injection 
+   * http://localhost:8080 or whatever the post controller url is
+   * Ergin
+   *  whenever they get the REST controller if there is a url for the GET, POST, UPDATE, DELETE just add
+   *  the url as private baseURL = "URL of the method" and thats it and we can work on the functions
+   *  together.
+  */
+  private baseURL = ""
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * When they get the controller up we need to match each function to the function in the controller
+  */
 }
