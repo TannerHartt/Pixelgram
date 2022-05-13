f3d1d7d619903e1fc18984015fb8220847ca3801
Granger Carty <2185580@cognizant.com>
2022-05-13 15:33:16 -0500

    Created skeleton for FeedComponent class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from '../components/post/post.component';
-import { Feed2Component } from './feed2/feed2.component';
 
 @NgModule({
  imports: [
@@ -12,7 +11,6 @@ import { Feed2Component } from './feed2/feed2.component';
   declarations: [
     AppComponent,
     PostComponent,
-    Feed2Component
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit } from '@angular/core';
+import { PostComponent } from 'src/components/post/post.component';
+
+@Component({
+    selector: 'app-feed-component',
+    templateUrl: './feed.component.html',
+    styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+
+    posts!: PostComponent[];
+
+    ngOnInit(): void {
+        this.loadFivePostCompents;
+    }
+
+    loadFivePostCompents() {
+        // This is where logic for loading PostComponents into posts goes
+    }
+
+    onNextClick() {
+        this.loadFivePostCompents;
+    }
+}
