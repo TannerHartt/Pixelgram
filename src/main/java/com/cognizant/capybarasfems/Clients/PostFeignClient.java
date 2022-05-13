c258cfea534f0e24d9c430151b043ccdbcf02139
waaik.collier-perrin <waaik.collier-perrin@cognizant.com>
2022-05-13 18:30:00 -0400

    Started Feign Client implementation

---
+++ src/main/java/com/cognizant/capybarasfems/Clients/PostFeignClient.java
@@ -0,0 +1,21 @@
+package com.cognizant.capybarasfems.Clients;
+
+import com.cognizant.capybarasfems.Models.PageOfItems;
+import com.cognizant.capybarasfems.Models.Post;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//Needs value of Post-CRUD and corresponding Post URL
+@FeignClient(value = "", url = "")
+public interface PostFeignClient {
+   //
+    @GetMapping("/posts?pageNumber={int}&pageSize={int})")
+    PageOfItems<Post> getPost(@PathVariable int pageNumber, @PathVariable int pageSize);
+
+    //Sends Post from PostController
+    @PostMapping("")
+    Post addPost(@RequestBody Post post);
+
+}
