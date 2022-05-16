b7f6616c2e491e420e748a1462dd0992dfdbffb6
waaik.collier-perrin <waaik.collier-perrin@cognizant.com>
2022-05-16 11:26:08 -0400

    Revamped PostService Class, added PostServiceInterface, and added more tests. Added Small change

--- src/test/java/com/cognizant/Post/ServiceTest/PostServiceTest.java
+++ src/test/java/com/cognizant/Post/ServiceTest/PostServiceTest.java
@@ -64,7 +64,7 @@ public class PostServiceTest {
         pageOfItems = new PageOfItems(postList, true, 6);
 
     }
-
+// Orignal Test
    /* @Test
     public void isGetPageItems() {
         ArrayList<Post> postArrayList = new ArrayList<Post>();
