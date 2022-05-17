3b82040fadf15c42ea8b72a874993abae2135747
2185584 <2185584@cognizant.com>
2022-05-17 08:56:47 -0700

    Work on post component layout

--- src/components/post/post.component.ts
+++ src/components/post/post.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post-service.service';
 
 @Component({
-  selector: 'app-post-component',
+  selector: '.app-post-component',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postServiceService: PostServiceService) { }
 
   ngOnInit(): void {
+
   }
 
 }
