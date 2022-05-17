cd4fdc9c57a1ea2a117a4cf70f7c37c7255dfe74
Bostanci <2185581@cognizant.com>
2022-05-17 08:32:17 -0500

    Branch

--- src/app/PostUIService/postuiservice.service.spec.ts
+++ src/app/PostUIService/postuiservice.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PostuiserviceService } from './postuiservice.service';
 
 describe('PostuiserviceService', () => {
   let service: PostuiserviceService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PostuiserviceService]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(PostuiserviceService);
   });
 
