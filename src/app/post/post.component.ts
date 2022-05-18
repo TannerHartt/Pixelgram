9e458d2f73437c4c264a5d9ca1d26e20fa9f7f1b
2185584 <2185584@cognizant.com>
2022-05-18 12:18:56 -0700

    Fixed component issues and import issues.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/models/post';
+import { User } from 'src/models/user';
 import { PostServiceService } from 'src/services/post-service/post-service.service';
 
 @Component({
-  selector: 'app-post-component',
-  templateUrl: 'post.component.html',
-  styleUrls: ['post.component.css']
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
 
   constructor(private postServiceService: PostServiceService) { 
     this.postServiceService = postServiceService;
-  
   }
+
+  
+  User = new User();
 
   Post = new Post();
-
   ngOnInit(): void {
-    
   }
 
-  
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,11 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  templateUrl: 'user.component.html',
-  styleUrls: ['user.component.css']
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  
 
   constructor() { }
 
