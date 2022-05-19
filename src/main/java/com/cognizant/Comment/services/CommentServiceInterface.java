f8205d79160f29a27c65278524c6036441e08a6c
Brian White <brian.white@cognizant.com>
2022-05-19 11:44:16 -0700

    Add interface, implementation, repo. Passed test.

---
+++ src/main/java/com/cognizant/Comment/services/CommentServiceInterface.java
@@ -0,0 +1,13 @@
+package com.cognizant.Comment.services;
+
+import com.cognizant.Comment.models.Comment;
+import com.cognizant.Comment.models.PageOfItems;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CommentServiceInterface {
+    PageOfItems<Comment> getAllComment(int pageNumber, int pageSize);
+    Page<Comment> getCommentPage(int pageNumber, int pageSize);
+    Object addComment(Comment comment);
+}
