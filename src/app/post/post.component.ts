552103323bc35c4600bacc4e3b97da50e406f950
2185584 <2185584@cognizant.com>
2022-05-19 15:17:21 -0700

    More work in progress on the post component.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { PageOfItems } from 'src/models/page-of-item';
 import { Post } from 'src/models/post';
 import { User } from 'src/models/user';
 import { PostServiceService } from 'src/services/post-service/post-service.service';
@@ -10,12 +12,51 @@ import { PostServiceService } from 'src/services/post-service/post-service.servi
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
-  
   User = new User();
   Post = new Post();
+
+  posts!: Post[];
+
+  constructor(private postServiceService: PostServiceService) { 
+    this.postServiceService = postServiceService;
+  }
+   postService!: PostServiceService
+
+   
+  
+
+    user1: User = {
+      id: 0,
+      username: " ",
+      profileImg: " ",
+    } 
+
+    commentPage: PageOfItems<Comment> = {
+      items: [],
+      hasNext: false,
+      totalElements: 0
+    }
+
+    post: Post = {
+      id: 0,
+      user: this.user1,
+      img: " ",
+      description: " ",
+      createdOn: new Date(),
+      comments: []
+    }
+    pageNumber!: number;
+    pageSize!: number;
+
+    
+
   
   ngOnInit(): void {
+    this.fetchPagedPosts();
   }
+
+
+  this.postService.fetchPagedPosts()
+  
 
 }
