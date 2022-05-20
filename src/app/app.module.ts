9fef9500eb889b9819e5ffcd6c6e9345aa8ffb66
Granger Carty <2185580@cognizant.com>
2022-05-20 11:17:04 -0500

    Fixed app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { PostComponent } from '../components/post/post.component';
-import { UserComponent } from '../components/user/user.component';
-import { LandingPageComponent } from '../components/landing-page/landing-page.component';
-import { FeedComponent } from 'src/components/feed/feed.component';
-=======
 import { PostComponent } from './post/post.component';
 import { UserComponent } from './user/user.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FeedComponent } from './feed/feed.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
->>>>>>> 8d58458baf3437e4ad24c8fbc956717432d294dd
 
 @NgModule({
  imports: [
@@ -22,12 +17,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppComponent,
     PostComponent,
     UserComponent,
-<<<<<<< HEAD
-    LandingPageComponent,
-    FeedComponent
-=======
-    NavBarComponent
->>>>>>> 8d58458baf3437e4ad24c8fbc956717432d294dd
+    NavBarComponent,
+    FeedComponent,
+    LandingPageComponent
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
