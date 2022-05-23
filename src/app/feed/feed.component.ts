a93cf341305624010dc23846308279ca06417df9
Granger Carty <2185580@cognizant.com>
2022-05-23 14:43:55 -0500

    Got landing page connected

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit } from '@angular/core';
-import { PostComponent } from 'src/components/post/post.component';
+import { PostServiceService } from 'src/services/post-service/post-service.service';
+import { PageOfItems } from 'src/models/page-of-item';
+import { PostUI } from 'src/models/PostUI';
 
 @Component({
     selector: 'app-feed-component',
@@ -8,14 +10,23 @@ import { PostComponent } from 'src/components/post/post.component';
 })
 export class FeedComponent implements OnInit {
 
-    posts!: PostComponent[];
+    posts!: PostUI[];
+    postData = new PageOfItems<PostUI>();
+
+    constructor (private postServiceService: PostServiceService) {}
+
+    pageNumber: number = 0;
+    pageSize: number = 5;
 
     ngOnInit(): void {
         this.loadFivePostCompents();
     }
 
     loadFivePostCompents() {
-        // This is where logic for loading PostComponents into posts goes
+        this.postServiceService.fetchListOfPosts(this.pageNumber, this.pageSize).subscribe(data => {
+            this.postData = data;
+            console.log(this.postData.items);
+        });
     }
 
     onNextClick() {
