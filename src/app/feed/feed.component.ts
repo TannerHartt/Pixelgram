79e75224f2d8e6c9f457e0ae2d4ce128f04a4517
Tanner.Hart <tanner.hart@cognizant.com>
2022-05-24 00:10:13 +0000

    Feature/infinity scrolls

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit } from '@angular/core';
 import { PostServiceService } from 'src/services/post-service/post-service.service';
 import { PageOfItems } from 'src/models/page-of-item';
 import { PostUI } from 'src/models/PostUI';
@@ -17,19 +17,31 @@ export class FeedComponent implements OnInit {
 
     pageNumber: number = 0;
     pageSize: number = 5;
+    @Input() 
+    distance: number = .1;
+    @Input()
+    throttle: number = 300;
+   
+
 
     ngOnInit(): void {
-        this.loadFivePostCompents();
+        this.loadFivePostComponents();
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
     }
 
-    loadFivePostCompents() {
+    loadFivePostComponents() {
         this.postServiceService.fetchListOfPosts(this.pageNumber, this.pageSize).subscribe(data => {
             this.postData = data;
-            console.log(this.postData.items);
+            
         });
+        console.log(this.postData.items);
     }
 
-    onNextClick() {
-        this.loadFivePostCompents();
-    }
+   onScrollDown() {
+    this.postServiceService.fetchListOfPosts(this.pageNumber, this.pageSize += 5).subscribe(data =>
+        {this.postData = data;});
+    console.log(this.postData)
+   }
 }
