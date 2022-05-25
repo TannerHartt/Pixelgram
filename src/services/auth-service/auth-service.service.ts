d1040624083e549b8131e8963f719c5ab0038ab7
Brian White <brian.white@cognizant.com>
2022-05-25 14:02:26 -0700

    forgot to add file

--- src/services/auth-service/auth-service.service.ts
+++ src/services/auth-service/auth-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthResponse } from 'src/models/auth-response';
+import { AuthReq } from 'src/models/auth-req';
+import { Observable } from 'rxjs/internal/Observable';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
+  baseURL: any;
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  userLogin(authreq: AuthReq):Observable<AuthResponse> {
+    return this.http.get<AuthResponse>(`${this.baseURL}?username=${authreq.username}&password=${authreq.password}&client_id=${authreq.client_id}&grant_type=${authreq.grant_type}`);
+  }
 }
