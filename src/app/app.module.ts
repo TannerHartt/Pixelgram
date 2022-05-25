323682232d247eaf7a35c2bdba762408239559ca
Carlos Hernandez Espinoza <carlos.hernandezespinoza@cognizant.com>
2022-05-25 16:25:51 -0500

    routing between landing page and login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,21 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FeedComponent } from './feed/feed.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
  imports: [
     BrowserModule,
     HttpClientModule,
-    InfiniteScrollModule
+    InfiniteScrollModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: [
     AppComponent,
@@ -22,7 +31,9 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
     NavBarComponent,
     FeedComponent,
     LandingPageComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginPageComponent,
+    LoginComponentComponent
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
