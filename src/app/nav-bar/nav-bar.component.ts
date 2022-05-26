5220929e1e5b994e603931b2668a127e677e8a95
2185584 <2185584@cognizant.com>
2022-05-25 21:30:30 -0700

    Created a lgoin button to appear on user icon click

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,12 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
+
+  Login: boolean = true;
+  isVisible: boolean = false;
+
 
   goTo(url: any) {
     window.location.href = url;
   }
 
+  refreshPage() {
+    window.location.reload();
+  }
+
+  showLogin() {
+    this.Login = !this.Login;
+    this.isVisible = !this.isVisible;
+  }
 }
