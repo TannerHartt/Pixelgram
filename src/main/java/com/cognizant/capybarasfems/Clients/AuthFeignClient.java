6ee25d69bd1ab7a6c4f31c52b831435db9965acb
Justin Freeman <justin.freeman@cognizant.com>
2022-05-27 14:38:01 -0500

    Fixed bad Url link, changed getmapping to post mapping

--- src/main/java/com/cognizant/capybarasfems/Clients/AuthFeignClient.java
+++ src/main/java/com/cognizant/capybarasfems/Clients/AuthFeignClient.java
@@ -3,12 +3,13 @@ package com.cognizant.capybarasfems.Clients;
 import com.cognizant.capybarasfems.Models.AuthRequest;
 import com.cognizant.capybarasfems.Models.AuthResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-@FeignClient(value= "AuthClient", url="https://enablement-keycloak.work.cognizant.studio//auth/realms/SpringBootKeycloak/protocol/openid-connect/token")
+//fetching the api token from keycloak for authentication of client
+@FeignClient(value= "AuthClient",
+        url="https://enablement-keycloak.work.cognizant.studio/auth/realms/Enablement/protocol/openid-connect/token")
 public interface AuthFeignClient {
-    @GetMapping("/token")
+    @PostMapping
     AuthResponse AuthClient(@RequestBody AuthRequest body);
 }
